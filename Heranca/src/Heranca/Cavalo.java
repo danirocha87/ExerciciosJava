c8984990c9be48a575dc76324c478fbafffe283e
daniela oliveira <danioliver_17@hotmail.com>
2022-09-16 18:59:30 -0300

    Exercicio de Herança

---
+++ Heranca/src/Heranca/Cavalo.java
@@ -0,0 +1,20 @@
+package Heranca;
+
+public class Cavalo extends Animal{
+
+	private boolean corre;
+
+	public Cavalo(String nome, int idade, boolean emiteSom, String som, boolean corre) {
+		super(nome, idade, emiteSom, som);
+		this.corre = corre;
+	}
+
+	public void acao() {
+		if (corre == true) {
+			System.out.println("O animal deve correr.");
+		}else {
+			System.out.println("O animal não deve correr.");
+		}
+	
+	}
+}
